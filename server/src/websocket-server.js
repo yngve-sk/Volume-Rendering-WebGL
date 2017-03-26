e415dcac44512126d400d31c394e18a3ee66e864
Yngve Sekse Kristiansen <ykristiansen@live.no>
2017-03-27 01:10:39 +0200

    Simplified some

--- server/src/websocket-server.js
+++ server/src/websocket-server.js
@@ -20,22 +20,28 @@ for (let i = 0; i < 10000000; i++)
     smaller[i] = 5;
 
 wss.on('connection', function connection(ws) {
-
     let requests = [];
 
     ws.on('message', (message) => {
         const json = JSON.parse(message);
         const type = json.type;
+        const doStringify = json.doStringify; // 'json' or 'buffer'
 
         switch (type) {
             case 'get':
                 const resource = json.resource;
-                switch (resource) {
-                    case 'isovalues':
-                        console.log("sending the isovalues!");
-                        ws.send(theDatasets.hand.isovalues);
-                        break;
-                }
+                console.log("GET: " + resource);
+                console.log("TYPE: " + json.type);
+                console.log("do stringify? " + doStringify);
+                console.log("Sending object...");
+
+                let toSend = theDatasets.manix[resource];
+                if (doStringify)
+                    toSend = JSON.stringify(toSend);
+
+                //console.log(toSend);
+                ws.send(toSend);
+                console.log("Sent object!");
                 break;
         }
 
